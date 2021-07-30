cce716c0bc107b59d2ed88bba5dfde6ee589daff
lincoln <lincoln-md@hotmail.com>
2021-07-29 22:45:01 -0300

    alterações de layout + responsividade em index e contatos

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@
         } 
     });  
   });
+
+  $(document).ready(function() {
+    $('#autoWidth3').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+  
